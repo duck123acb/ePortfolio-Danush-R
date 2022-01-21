e31f5e6ed9f8a157f9c0c3598f5e36de8ce7d444
Le Duck <95136800+duck123acb@users.noreply.github.com>
2022-01-21 06:21:08 -0700

    changed size of peanut butter

--- aboutMeScript.js
+++ aboutMeScript.js
@@ -5,5 +5,5 @@ jif.addEventListener("click", function() {
     replace.innerText = "Peanut Butter";
     replace.style.backgroundColor = "#bf834e";
     replace.style.color = "#FFFFFF";
-    replace.style.fontSize = "400px";
+    replace.style.fontSize = "300px";
 });
