91f6499f9dc5b7f80a634d1669d3d757a640b041
Le Duck <95136800+duck123acb@users.noreply.github.com>
2022-01-21 06:18:07 -0700

    p e a n u t b u t t e r

---
+++ aboutMeScript.js
@@ -0,0 +1,8 @@
+const jif = document.getElementById("jif");
+const replace = document.getElementById("replace");
+
+jif.addEventListener("click", function() {
+    replace.innerText = "Peanut Butter";
+    replace.style.backgroundColor = "#bf834e";
+    replace.style.color = "#FFFFFF"
+});
